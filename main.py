a7150a610dd22240956337470734fa825046ef80
Alejandro F <afuste7@gmail.com>
2024-09-17 22:28:32 -0400

    imported read_grayscale_image file and printed matrix

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from src.utils.read_grayscale_image import read_grayscale_image
+
+image_path = "data/images/golfer_grayscale.jpg"
+
+I = read_grayscale_image(image_path)
+
+print(I)
+
+
--- src/utils/read_grayscale_image.py
+++ src/utils/read_grayscale_image.py
@@ -9,4 +9,7 @@ def read_grayscale_image(image_path):
 
     I = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
+    if I is None:
+        raise ValueError("Image not found or unable to read.")
+
     return I
