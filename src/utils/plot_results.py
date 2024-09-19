156b132dcbb62379f8e144410243631df73f9bc3
Alejandro F <afuste7@gmail.com>
2024-09-19 19:37:53 -0400

    added preprocessing.py file with import statement for math library

---
+++ src/utils/plot_results.py
@@ -0,0 +1,14 @@
+from src.utils.read_grayscale_image import read_grayscale_image
+import matplotlib.pyplot as plt
+
+
+# path to golfer grayscale image
+image_path = "../../data/images/golfer_grayscale.jpg"
+
+# function call creates matrix
+matrix = read_grayscale_image(image_path)
+
+# Display the grayscale matrix as an image
+plt.imshow(matrix, cmap="gray", vmin=0, vmax=255)
+plt.colorbar()
+plt.show()
