2057061396b4091cd6010903e877a9fe60b9fa66
Alejandro F <afuste7@gmail.com>
2024-09-19 16:21:15 -0400

    added code to test read_grayscale_image function

--- src/utils/read_grayscale_image.py
+++ src/utils/read_grayscale_image.py
@@ -1,4 +1,6 @@
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
 def read_grayscale_image(image_path):
     """
@@ -13,3 +15,20 @@ def read_grayscale_image(image_path):
         raise ValueError("Image not found or unable to read.")
 
     return I
+
+# code to make sure function works remove 
+
+'''
+
+# path to golfer grayscale image
+image_path = "../../data/images/golfer_grayscale.jpg"
+
+# function call creates matrix
+matrix = read_grayscale_image(image_path)
+
+# Display the grayscale matrix as an image
+plt.imshow(matrix, cmap="gray", vmin=0, vmax=255)
+plt.colorbar()
+plt.show()
+
+'''
--- tests/utils/read_grayscale_image.py
+++
@@ -1,3 +0,0 @@
-from src.utils.read_grayscale_image import read_grayscale_image
-import matplotlib.pyplot as plt
-import numpy as np
