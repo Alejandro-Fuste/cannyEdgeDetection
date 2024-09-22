30ccbad76494b1dda86c9778295e1e1f4c6745fd
Alejandro F <afuste7@gmail.com>
2024-09-22 19:14:06 -0400

    created a separate file for creating a gaussian kernel

--- src/gaussian_function.py
+++ src/gaussian_function.py
@@ -1,6 +1,6 @@
 import math
 
-# Define the 1D Gaussian function
+# Define the 1D Gaussian function using the gaussian equation
 def gaussian_function(x, sigma):
     exponent = - (x ** 2) / (2 * sigma ** 2)
     return math.exp(exponent)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,32 +1,8 @@
-import math
 from src.gaussian_function import gaussian_function
+from src.gaussian_kernel import create_gaussian_kernel
 
-# Define the 1D Gaussian function
-def gaussian_function(x, sigma):
-    exponent = - (x ** 2) / (2 * sigma ** 2)
-    return math.exp(exponent)
 
-# Create the 3x3 Gaussian kernel
-def create_gaussian_kernel(size, sigma):
-    kernel = []
-    kernel_sum = 0 # For normalizing kernel
-    offset = size//2 # To keep the kernel around 0
 
-    for i in range(-offset, offset + 1):
-        row = []
-        for j in range(-offset, offset + 1):
-          # Calculate the 2D Gaussian value (since this is a 2D kernel)
-          value = gaussian_function(i, sigma) * gaussian_function(j, sigma)
-          row.append(value)
-          kernel_sum += value
-        kernel.append(row)
-
-   # normalize kernel
-    for i in range(size):
-        for j in range(size):
-            kernel[i][j] /= kernel_sum
-
-    return kernel
 
 size = 3
 sigma = 1
