a04dd95a6f06285d1fefc4aad7ec22ed612d6eab
Alejandro F <afuste7@gmail.com>
2024-09-22 20:23:59 -0400

    added nested loops code to generate kernels in x and y direction

--- src/derivative_gaussian_kernel.py
+++ src/derivative_gaussian_kernel.py
@@ -2,4 +2,15 @@ from src.gaussian_derivative import gaussian_derivative
 from src.gaussian_function import gaussian_function
 
 def generate_derivative_gaussian_kernel(size, sigma):
+    kernel_x = [[0 for _ in range(size)] for _ in range(size)]
+    kernel_y = [[0 for _ in range(size)] for _ in range(size)]
+
+    center = size // 2
+    for i in range(size):
+        for j in range(size):
+            x = i - center
+            y = j - center
+            kernel_x[i][j] = gaussian_derivative(x, sigma) * gaussian_function(y, sigma)
+            kernel_y[i][j] = gaussian_function(x, sigma) * gaussian_derivative(y, sigma)
     
+    return kernel_x, kernel_y
