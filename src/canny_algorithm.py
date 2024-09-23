4da22a0e4d2b62b63ca44baa7df534aeae8a2bed
Alejandro F <afuste7@gmail.com>
2024-09-23 16:29:33 -0400

    added step for computing magnitude in canny file

--- src/canny_algorithm.py
+++ src/canny_algorithm.py
@@ -1,6 +1,7 @@
 from src.convolution import convolve_image_with_kernel
 from src.preprocessing import gaussian_kernel
 from src.preprocessing import x_derivative, y_derivative
+from src.magnitude import compute_gradient_magnitude
 
 
 def canny_edge_detector(image):
@@ -11,3 +12,6 @@ def canny_edge_detector(image):
     x_derivative_image = convolve_image_with_kernel(filtered_image, x_derivative)
     y_derivative_image = convolve_image_with_kernel(filtered_image, y_derivative)
 
+    # Compute magnitude
+    magnitude = compute_gradient_magnitude(x_derivative_image, y_derivative_image, 1.5)
+    
