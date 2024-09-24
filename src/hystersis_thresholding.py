6cc0f5fba238936172bbe14b0d8b889c7cef9d5c
Alejandro F <afuste7@gmail.com>
2024-09-23 21:22:20 -0400

    added code for setting empty array, getting strong and weak pixels

--- src/hystersis_thresholding.py
+++ src/hystersis_thresholding.py
@@ -11,10 +11,15 @@ strong_pixel = 255
 
 def hysteresis_thresholding(gradient_magnitude):
     # Create an empty array for the result
+    result = np.zeros_like(gradient_magnitude, dtype=np.uint8)
 
     # Mark the strong pixels
+    strong_row, strong_col = np.where(gradient_magnitude >= high_threshold)
+    result[strong_row, strong_col] = strong_pixel
 
     # Mark the weak pixels
+    weak_row, weak_col = np.where((gradient_magnitude >= low_threshold) & (gradient_magnitude < high_threshold))
+    result[weak_row, weak_col] = weak_pixel
 
     # Remove weak pixels unless connected to strong pixels
 
